eae4bf2aaaff2a97d97934255204d9ee11034028
weimin yang <yangwm1983@gmail.com>
2019-08-02 09:57:11 -0700

    Updates

---
+++ precache-manifest.0d10df6f34a53f68f53a2600a005b949.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d86ea4ce8b972e7a66f4b45375c21c31",
+    "url": "/clickit/index.html"
+  },
+  {
+    "revision": "735ef6644c6800db4541",
+    "url": "/clickit/static/css/main.999055cf.chunk.css"
+  },
+  {
+    "revision": "fc8711c2b43a24f9cbb6",
+    "url": "/clickit/static/js/2.d0798c63.chunk.js"
+  },
+  {
+    "revision": "735ef6644c6800db4541",
+    "url": "/clickit/static/js/main.c6e84689.chunk.js"
+  },
+  {
+    "revision": "0c339fd0654aba25cf6c",
+    "url": "/clickit/static/js/runtime~main.37a4a718.js"
+  }
+]);
